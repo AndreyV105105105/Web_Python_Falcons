9accbb27f3585fd3b0b86af707b2f0e3b582a69d
Yegor <egor-epeggar-meks@yandex.ru>
2026-03-24 20:56:04 +0300

    add viewsets and filters

--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """ViewSet для категорий"""
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """ViewSet для товаров с фильтрацией, поиском и сортировкой"""
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    filter_backends = [
+        DjangoFilterBackend, 
+        filters.SearchFilter, 
+        filters.OrderingFilter
+    ]
+    
+    filterset_fields = ['category', 'is_available', 'price']
+    search_fields = ['name', 'description']
+    ordering_fields = ['price', 'created_at']
