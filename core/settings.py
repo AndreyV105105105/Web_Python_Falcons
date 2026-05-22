d2013cd3c4f434842f7f0afd6f51141e981c3c51
BagiCafe <dalekseev555@gmail.com>
2026-05-22 18:41:03 +0300

    configure core settings for product existence check API

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-debug-key-for-local-testin
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-web', '*']
 
 
 # Application definition
